3d40ce9830ea9a04cc3d310f4a6457127d72be14
caro <cirier.c@gmail.com>
2025-03-14 17:25:31 +0100

    project including Dockerfile

---
+++ FRONT/mon_projet_next/pages/api/test.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function TestAPI() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    //fetch(process.env.NEXT_PUBLIC_API_URL + "/api/home/")
+    fetch("http://backend:8000/api/home/")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Erreur API:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Test API Django</h1>
+      {data ? <p>Réponse : {JSON.stringify(data)}</p> : <p>Chargement...</p>}
+    </div>
+  );
+}
---
+++ FRONT/mon_projet_next/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Bienvenue sur mon projet Next.js ! 🎉</h1>
+        <p>Lalala Accédez au test de l'API Django ici : <a href="/api/test">/api/test</a></p>
+      </div>
+    );
+  }
+  
