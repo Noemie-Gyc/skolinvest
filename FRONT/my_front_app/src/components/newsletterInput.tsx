c0c945469c6dd8b06c004a7e7da436282c52b182
Noemie-Gyc <goyec.noemie@gmail.com>
2025-06-03 14:51:45 +0200

    Feature-52--Ajout du footer

--- FRONT/my_front_app/src/components/newsletterInput.tsx
+++ FRONT/my_front_app/src/components/newsletterInput.tsx
@@ -1,6 +1,11 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Mails } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 type Props = {
     placeholder?: string,
@@ -10,18 +15,23 @@ type Props = {
 
 export function NewsletterInput({placeholder} : Props) {
     return (
-        <div className="border rounded-lg flex justify between items-center">
+        <div className="border-none rounded-lg flex justify between items-center gap-x-2">
             <Input
                 type ="text"
                 placeholder={placeholder || "you@example.com"}
-                className = "border-none"
+                className = "bg-white placeholder-black text-black border-none rounded-md px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-primary flex-1/2"
             />
-            <Button variant ="ghost" size="icon">
-                <Mails/>
-                <span className="sr-only"> Newsletter Button </span>
-            </Button>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                        <Mails />
+                        <span className="sr-only">Envoyer</span>
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Inscription à la newsletter</p>
+                </TooltipContent>
+            </Tooltip>
         </div>
     )
-
-
 }
